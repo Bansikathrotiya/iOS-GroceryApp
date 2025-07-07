0e51928ed01694c519adbf4cce239c49780d2e25
Bansikathrotiya <bansikathrotiya58@gmail.com>
2025-07-07 12:37:47 +0530

    code

--- GroceryApp/System/VC/TabBar/AccountVC.swift
+++ GroceryApp/System/VC/TabBar/AccountVC.swift
@@ -8,11 +8,32 @@
 import UIKit
 
 class AccountVC: UIViewController {
-
+    
+    //MARK: - Outlets
+    @IBOutlet weak var lblSignout: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(showSignOut))
+        self.lblSignout.isUserInteractionEnabled = true
+        self.lblSignout.addGestureRecognizer(tap)
+        
+    }
+    
+    //MARK: - Sign Out Confirmation
+    @objc func showSignOut() {
+        let alert = UIAlertController(title: "Sign Out", message: "Are you sure you want to sign out?", preferredStyle: .alert)
+        let clickOnCancel = UIAlertAction(title: "Cancel", style: .cancel)
+        let clickOnSignout = UIAlertAction(title: "Sign Out", style: .destructive) { _ in
+            let onboradingVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "OnBoradingVC")
+            self.navigationController?.setViewControllers([onboradingVC], animated: true)
+        }
+        alert.addAction(clickOnCancel)
+        alert.addAction(clickOnSignout)
+        present(alert, animated: true)
     }
     
     //MARK: - Actions
@@ -21,7 +42,7 @@ class AccountVC: UIViewController {
         self.navigationController?.pushViewController(aboutmeVC, animated: true)
     }
     
-
+    
     @IBAction func clickOnNotification(_ sender: Any) {
         let notificationVC = UIStoryboard(name: "Account", bundle: nil).instantiateViewController(withIdentifier: "NotificationVC")
         self.navigationController?.pushViewController(notificationVC, animated: true)
